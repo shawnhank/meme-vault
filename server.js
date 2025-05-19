b3961974f7a271431ee6f81fb5ba7ed01c23ff40
Shawn Hank <shawn@shawnhank.com>
2025-05-18 19:46:25 -0600

    added authRoutes and mounted auth routes at root for /login, /register and /logout functions. part of refactoring app to meet unit 2 project requirements.

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const session = require('express-session'); // store session data server-side
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const Meme = require('./models/meme');
-const memeRoutes = require('./routes/memes');
+const authRoutes = require('./routes/auth');   // Import login/register/logout routes
+const memeRoutes = require('./routes/memes');  // IMport meme routhes
+const userRoutes = require('./routes/users');  // Import user profile routes
 
 // create app listening port
 const port = process.env.PORT || 3000;
@@ -57,56 +59,15 @@ app.use((req, res, next) => {
 
 
 // Routers
-app.use('/memes', memeRoutes);
-
-
-// This route gets all memes from the database and also passes along
-// any query string data (like ?deleted=true) to the template.
-// The view uses that to optionally show feedback messages to the user (e.g. delete successful)
-// app.get('/memes', async (req, res) => {
-//   const memes = await Meme.find();
-//   res.render('memes/index', { memes, query: req.query });
-// });
+app.use('/', authRoutes);       // Mount auth routes at root (e.g. /login, /register, /logout)
+app.use('/users', userRoutes);  // mount user profile routes
+app.use('/memes', memeRoutes);  // mount meme routes
 
 // Get /   Memes index (list of all memes)
 app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
 
-// GET /memes/new   New Form
-// app.get('/memes/new', (req, res) => {
-//   res.render('memes/new');
-// });
-
-// POST /memes  CREATE function
-// app.post('/memes', async (req, res) => {
-//   await Meme.create(req.body);
-//   res.redirect('/memes');
-// });
-
-// GET memes/:id SHOW function
-// app.get('/memes/:id', async (req, res) => {
-//   const meme = await Meme.findById(req.params.id);
-//   res.render('memes/show', { meme });
-// });
-
-// GET /memes/:id/edit  EDIT Action
-// app.get('/memes/:id/edit', async (req, res) => {
-//   const meme = await Meme.findById(req.params.id);
-//   res.render('memes/edit', { meme });
-// });
-
-// PUT /memes/:id  UPDATE Action
-// app.put('/memes/:id', async (req, res) => {
-//   await Meme.findByIdAndUpdate(req.params.id, req.body);
-//   res.redirect(`/memes/${req.params.id}`);
-// });
-
-// DELETE /memess/:id  DELETE Action
-// app.delete('/memes/:id', async (req, res) => {
-//   await Meme.findByIdAndDelete(req.params.id);
-//   res.redirect('/memes?deleted=true');
-// });
 
 // Listen for incoming requests on port 3000
 app.listen(port, () => {
