a7a305f86d184429a81b3e87458bbe77028ccc7d
Shawn Hank <shawn@shawnhank.com>
2025-05-20 01:00:34 -0600

    added  isLoggedIn, to all user and meme functions that require sign in (aka protected routes/features) with a redirect to the register page to sign up for an account.

--- routes/memes.js
+++ routes/memes.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const memesCtrl = require('../controllers/memes');
+const { isLoggedIn } = require('../middleware/auth'); // to prevent a visitor from adding, editing, deleting memes
 
 // Index – GET /memes
 router.get('/', memesCtrl.index);
 
 // New – GET /memes/new
-router.get('/new', memesCtrl.newForm);
+router.get('/new', isLoggedIn, memesCtrl.newForm);
 
 // Create – POST /memes
-router.post('/', memesCtrl.create);
+router.post('/', isLoggedIn, memesCtrl.create);
 
 // Show – GET /memes/:id
 router.get('/:id', memesCtrl.show);
 
 // Edit – GET /memes/:id/edit
-router.get('/:id/edit', memesCtrl.editForm);
+router.get('/:id/edit', isLoggedIn, memesCtrl.editForm);
 
 // Update – PUT /memes/:id
-router.put('/:id', memesCtrl.update);
+router.put('/:id', isLoggedIn, memesCtrl.update);
 
 // Delete – DELETE /memes/:id
-router.delete('/:id', memesCtrl.destroy);
+router.delete('/:id', isLoggedIn, memesCtrl.destroy);
 
 module.exports = router;
