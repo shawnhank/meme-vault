cf0c8a6d3937147c9c92397641dc9eb5b28b45e0
Shawn Hank <shawn@shawnhank.com>
2025-05-21 14:48:50 -0600

    fixed formatting issues.

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,10 @@
-const express = require('express');                 // Load Express
-const router = express.Router();                    // Create the router
-const usersCtrl = require('../controllers/users');  // Import the users controller
-const { isLoggedIn } = require('../middleware/auth');  // to prevent a visitor from editing a form directly 
-
+const express = require('express'); // Load Express
+const router = express.Router(); // Create the router
+const usersCtrl = require('../controllers/users'); // Import the users controller
+const { isLoggedIn } = require('../middleware/auth'); // to prevent a visitor from editing a form directly
 
 // GET /community → Show all users in the app
-router.get('/community', isLoggedIn,  usersCtrl.listUsers);      // List all users with links to their profiles
+router.get('/community', isLoggedIn, usersCtrl.listUsers); // List all users with links to their profiles
 
 // GET /users/:id/edit → Show profile edit form
 router.get('/:id/edit', isLoggedIn, usersCtrl.editProfileForm);
@@ -14,12 +13,16 @@ router.get('/:id/edit', isLoggedIn, usersCtrl.editProfileForm);
 router.put('/:id', isLoggedIn, usersCtrl.updateProfile);
 
 // GET /users/:id/change-password → Show change password form
-router.get('/:id/change-password', isLoggedIn, usersCtrl.showChangePasswordForm);
+router.get(
+  '/:id/change-password',
+  isLoggedIn,
+  usersCtrl.showChangePasswordForm
+);
 
 // PUT /users/:id/change-password → Handle password update
 router.put('/:id/change-password', isLoggedIn, usersCtrl.updatePassword);
 
 // GET /users/:id → Show a single user's profile and their memes
-router.get('/:id', isLoggedIn, usersCtrl.showProfile);          // Load selected user + their memes
+router.get('/:id', isLoggedIn, usersCtrl.showProfile); // Load selected user + their memes
 
-module.exports = router;                            // Export router to use in server.js
+module.exports = router; // Export router to use in server.js
