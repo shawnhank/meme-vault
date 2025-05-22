387ea94730ef3e369ad5e1c2b9769c19c54793fe
Shawn Hank <shawn@shawnhank.com>
2025-05-21 18:00:31 -0600

    added models/favorites.js for a global favorites feature

---
+++ models/favorites.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const favoriteSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  meme: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Meme',
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+favoriteSchema.index({ user: 1, meme: 1 }, { unique: true });  // prevents duplicate favorites per user
+
+
+module.exports = mongoose.model('Favorite', favoriteSchema);
+
+
