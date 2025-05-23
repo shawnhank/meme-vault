9023f50397220e12baa0fcf9a3ce4d6e26dadd6e
Shawn Hank <shawn@shawnhank.com>
2025-05-23 06:13:35 -0600

    cleaning up code

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,8 @@
-// Middleware to protect routes from unauthenticated access
 function isLoggedIn(req, res, next) {
-  // does session user exist? is user logged in?
   if (!req.session.user) {
-    // If not show error and redirect to login
     req.flash('error', 'You must be logged in to do that. Please create an account to continue.');
     return res.redirect('/register'); 
   }
-
-  // If logged in, continue
   next();
 }
-
-// Export the middleware for use in routes
 module.exports = { isLoggedIn };
--- middleware/ownership.js
+++ middleware/ownership.js
@@ -1,32 +1,24 @@
-const Meme = require('../models/meme'); // Import the Meme model
+const Meme = require('../models/meme');
 
-// Middleware to check if the logged-in user owns the resource
 async function isOwner(req, res, next) {
   try {
-    // Find the meme by ID from the route parameter
     const meme = await Meme.findById(req.params.id);
 
-    // If no meme was found, show error and redirect
     if (!meme) {
       req.flash('error', 'Meme not found.');
-      return res.redirect('/memes'); //redirect to the memes index (/memes)
+      return res.redirect('/memes');
     }
 
-    // Check if the logged-in user is the one who created the meme
-    if (!meme.createdBy.equals(user._id)) {
+    if (!meme.createdBy.equals(req.session.user._id)) {
       req.flash('error', 'Not authorized.');
-      return res.redirect('/memes');  //redirect to the memes index (/memes)
+      return res.redirect('/memes');
     }
 
-    // If user is the owner, allow the request to proceed
     next();
   } catch (err) {
-    // If any error occurs, log it and redirect with an error message
-    console.log(err);
     req.flash('error', 'Permission check failed.');
-    res.redirect('/memes');  //redirect to the memes index (/memes)
+    res.redirect('/memes');
   }
 }
 
-// Export the middleware for use in routes
-module.exports = { isOwner }; 
+module.exports = { isOwner };
